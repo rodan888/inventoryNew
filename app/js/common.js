a1714db3587189e92cfbdf2688de12b1537dbb07
rodan8888 <tsumbaluk888@gmail.com>
2019-06-30 12:59:52 +0300

    add article page

--- app/js/common.js
+++ app/js/common.js
@@ -162,5 +162,36 @@ $(function() {
 		} catch(err) {
 
 		};
+
+		function videoStop($wrapper){
+			if (!$wrapper) {
+				var $wrapper = $('.js-videoWrapper');
+				var $iframe = $('.js-videoIframe');
+			} else {
+				var $iframe = $wrapper.find('.js-videoIframe');
+			}
+			$wrapper.removeClass('videoWrapperActive');
+			$iframe.attr('src','');
+		};
+		
+		function videoPlay($wrapper) {
+			var $iframe = $wrapper.find('.js-videoIframe');
+			var src = $iframe.data('src');
+			$wrapper.addClass('videoWrapperActive');
+			$iframe.attr('src',src);
+		}
+
+		$(document).on('click','.js-videoPoster',function(ev) {
+			ev.preventDefault();
+			var $poster = $(this);
+			var $wrapper = $poster.closest('.js-videoWrapper');
+			videoPlay($wrapper);
+		});
+		
+		$(document).on('click','.videoWrapperActive button',function(ev){
+			videoStop($(this).parent());
+		});
+
+
 	});
 });
